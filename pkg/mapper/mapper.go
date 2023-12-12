5acf0d5f8a05d124d515aecb5155191dc92001f5
Joseph Fourny <fourny@tehama.io>
2023-12-12 15:30:12 -0500

    Add `Increment` and `Decrement` mappers.

--- pkg/mapper/mapper.go
+++ pkg/mapper/mapper.go
@@ -234,3 +234,17 @@ func ComplexToComplex[E constraint.Complex, F constraint.Complex]() func(E) F {
 		return F(e)
 	}
 }
+
+// Increment returns a function that accepts a value of real number type E and returns the result of adding the provided `step` value to it.
+func Increment[E constraint.RealNumber](step E) func(E) E {
+	return func(e E) E {
+		return e + step
+	}
+}
+
+// Decrement returns a function that accepts a value of real number type E and returns the result of subtracting the provided `step` value from it.
+func Decrement[E constraint.RealNumber](step E) func(E) E {
+	return func(e E) E {
+		return e - step
+	}
+}
