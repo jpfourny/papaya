ad2e7e438f8e305592a634288a56dd1806520868
Joseph Fourny <fourny@tehama.io>
2023-12-12 22:31:40 -0500

    Add `SliceFrom/To` mapper operations

--- pkg/mapper/mapper.go
+++ pkg/mapper/mapper.go
@@ -248,3 +248,24 @@ func Decrement[E constraint.RealNumber](step E) func(E) E {
 		return e - step
 	}
 }
+
+// SliceFrom returns a function that accepts a slice of any type E and returns a slice of the same type containing all elements from the provided `start` index to the end of the slice.
+func SliceFrom[E any](start int) func([]E) []E {
+	return func(s []E) []E {
+		return s[start:]
+	}
+}
+
+// SliceTo returns a function that accepts a slice of any type E and returns a slice of the same type containing all elements from the start of the slice up to, but excluding, the provided `end` index.
+func SliceTo[E any](end int) func([]E) []E {
+	return func(s []E) []E {
+		return s[:end]
+	}
+}
+
+// SliceFromTo returns a function that accepts a slice of any type E and returns a slice of the same type containing all elements from the provided `start` index up to, but excluding, the provided `end` index.
+func SliceFromTo[E any](start int, end int) func([]E) []E {
+	return func(s []E) []E {
+		return s[start:end]
+	}
+}
