a73df8c1d9cfafef8dc248ef3ba67769e79b125b
jpfourny <jpfourny@gmail.com>
2024-01-10 22:22:12 -0500

    Move Increment/Decrement from math.go into misc.go.

--- pkg/mapper/misc.go
+++ pkg/mapper/misc.go
@@ -1,5 +1,7 @@
 package mapper
 
+import "github.com/jpfourny/papaya/pkg/constraint"
+
 // Identity returns a function that accepts a value of any type E and returns that value.
 func Identity[E any]() func(E) E {
 	return func(e E) E {
@@ -11,5 +13,19 @@ func Identity[E any]() func(E) E {
 func Constant[E any, F any](c F) func(E) F {
 	return func(E) F {
 		return c
+	}
+}
+
+// Increment returns a function that accepts a value of real number type E and returns the result of adding the provided `step` value to it.
+func Increment[E constraint.RealNumber](step E) func(E) E {
+	return func(e E) E {
+		return e + step
+	}
+}
+
+// Decrement returns a function that accepts a value of real number type E and returns the result of subtracting the provided `step` value from it.
+func Decrement[E constraint.RealNumber](step E) func(E) E {
+	return func(e E) E {
+		return e - step
 	}
 }
--- pkg/mapper/misc_test.go
+++ pkg/mapper/misc_test.go
@@ -21,3 +21,21 @@ func TestIdentity(t *testing.T) {
 		t.Errorf("Identity()(42) = %#v; want %#v", got, want)
 	}
 }
+
+func TestIncrement(t *testing.T) {
+	m := Increment[int](2)
+	got := m(42)
+	want := 44
+	if got != want {
+		t.Errorf("Increment(2)(42) = %#v; want %#v", got, want)
+	}
+}
+
+func TestDecrement(t *testing.T) {
+	m := Decrement[int](2)
+	got := m(42)
+	want := 40
+	if got != want {
+		t.Errorf("Decrement(2)(42) = %#v; want %#v", got, want)
+	}
+}
